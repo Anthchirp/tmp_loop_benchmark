25487f9e4bbef70385224b8dd641857a1bc54323
Nicholas Devenish <ndevenish@gmail.com>
2020-04-21 11:10:26 +0100

    Submodule (ugh)

--- bm.cxx
+++ bm.cxx
@@ -1,21 +1,32 @@
 #include <benchmark/benchmark.h>
 
+#include <scitbx/array_family/accessors/c_grid.h>
 #include <scitbx/array_family/tiny_types.h>
-// #include "dials/algorithms/image/threshold/local.h"
+#include <scitbx/array_family/versa.h>
+
+using namespace scitbx;
 
 static void BM_loop_inside(benchmark::State& state) {
+  af::versa<bool, af::c_grid<3> > base(af::c_grid<3>(af::tiny<int, 3>(14, 14, 5)));
+  af::versa<bool, af::c_grid<3> > m(base.accessor());
+
   for (auto _ : state) {
+    for (std::size_t j = 0; j < m.size(); ++j) {
+      benchmark::DoNotOptimize(m[j] = base[j]);
+    }
   }
 }
 BENCHMARK(BM_loop_inside);
 
 static void BM_loop_outside(benchmark::State& state) {
-  af::versa<bool, af::c_grid<3> > m(mask2.accessor());
-  DIALS_ASSERT(mask1.size() == mask2.size());
-  for (std::size_t j = 0; j < m.size(); ++j) {
-    m[j] = mask1[j] && mask2[j];
-  }
+  af::versa<bool, af::c_grid<3> > base(af::c_grid<3>(af::tiny<int, 3>(14, 14, 5)));
+  af::versa<bool, af::c_grid<3> > m(base.accessor());
+
   for (auto _ : state) {
+    const auto size = m.size();
+    for (std::size_t j = 0; j < size; ++j) {
+      benchmark::DoNotOptimize(m[j] = base[j]);
+    }
   }
 }
 BENCHMARK(BM_loop_outside);
